86ae82814e0f42dcd18f62e7a291adea629af9a6
dhwoo <lisp00@naver.com>
2024-03-11 20:31:04 +0900

    파라미터바인딩부터 시작

--- src/main/java/study/dataspa/repository/MemberRepository.java
+++ src/main/java/study/dataspa/repository/MemberRepository.java
@@ -1,9 +1,27 @@
 package study.dataspa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import study.dataspa.dto.MemberDto;
 import study.dataspa.entity.Member;
 
+import java.util.List;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+    // 실무에서는 메소드 이름으로 쿼리 생성 기능은 파라미터가 증가하면 메서드 이름이 매우 지저분해진다.
+    // 따라서 `@Query` 기능을 자주 사용하게 된다.
+    @Query("select m from Member m where m.username= :username and m.age = :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
+
+    @Query("select m.username from Member m")
+    List<String> findUsernameList();
+
+    @Query("select new study.dataspa.dto.MemberDto(m.id, m.username, t.name) from Member m join m.team t")
+    List<MemberDto> findMemberDto();
 
 
 }
--- src/test/java/study/dataspa/repository/MemberRepositoryTest.java
+++ src/test/java/study/dataspa/repository/MemberRepositoryTest.java
@@ -6,7 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import study.dataspa.dto.MemberDto;
 import study.dataspa.entity.Member;
+import study.dataspa.entity.Team;
+
+import java.util.List;
 
 @SpringBootTest
 @Transactional
@@ -14,6 +18,13 @@ import study.dataspa.entity.Member;
 public class MemberRepositoryTest {
     @Autowired
     MemberRepository memberRepository;
+
+    @Autowired
+    TeamRepository teamRepository;
+
+//    @Autowired
+//    memberRepository memberRepository;
+    
     @Test
     public void testMember() {
         Member member = new Member("memberB");
@@ -24,4 +35,61 @@ public class MemberRepositoryTest {
         Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
         Assertions.assertThat(findMember).isEqualTo(member); //JPA 엔티티 동일성 보장
      }
+
+    @Test
+    public void basicCRUD() {
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+            //단건 조회 검증
+        Member findMember1 = memberRepository.findById(member1.getId()).get();
+        Member findMember2 = memberRepository.findById(member2.getId()).get();
+        Assertions.assertThat(findMember1).isEqualTo(member1);
+        Assertions.assertThat(findMember2).isEqualTo(member2);
+
+        //리스트 조회 검증
+        List<Member> all = memberRepository.findAll();
+        Assertions.assertThat(all.size()).isEqualTo(2);
+
+        //카운트 검증
+        long count = memberRepository.count();
+        Assertions.assertThat(count).isEqualTo(2);
+
+        //삭제 검증
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
+        long deletedCount = memberRepository.count();
+        Assertions.assertThat(deletedCount).isEqualTo(0);
+    }
+
+
+    @Test
+    public void findUsernameList() {
+        Member member1 = new Member("AAA", 10);
+        Member member2 = new Member("BBB", 20);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        List<String> usernameList = memberRepository.findUsernameList();
+        for (String s : usernameList) {
+            System.out.println("S = " + s);
+        }
+    }
+
+    @Test
+    public void findMemberDto() {
+        Team team = new Team("teamA");
+        teamRepository.save(team);
+
+        Member member1 = new Member("AAA", 10);
+        member1.setTeam(team);
+        memberRepository.save(member1);
+
+        List<MemberDto> memberDto = memberRepository.findMemberDto();
+        for (MemberDto dto : memberDto) {
+            System.out.println(dto);
+        }
+    }
 }
